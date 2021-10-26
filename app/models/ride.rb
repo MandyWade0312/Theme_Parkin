7cef4c58d061b7017d9f9a70b989b396d96ccd59
MandyWade0312 <amanda.wade@kin.com>
2021-10-26 13:53:55 -0400

    updates

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -2,6 +2,8 @@ class Ride < ApplicationRecord
   belongs_to :park
   has_many :reviews, dependent: :destroy
 
-  geocoded_by :latitude, :longitude
-  after_validation :geocode
+  # geocoded_by :latitude, :longitude
+  # after_validation :geocode, if: :latitude_changed? 
+
+ 
 end
