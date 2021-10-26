c554383b4552577878cd78df912358e373d2fdf4
MandyWade0312 <amanda.wade@kin.com>
2021-10-26 13:52:00 -0400

    Added Users model, generated scaffolding

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_26_162110) do
+ActiveRecord::Schema.define(version: 2021_10_26_172422) do
 
   create_table "parks", force: :cascade do |t|
     t.string "name"
@@ -41,6 +41,13 @@ ActiveRecord::Schema.define(version: 2021_10_26_162110) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["park_id"], name: "index_rides_on_park_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   add_foreign_key "reviews", "rides"
