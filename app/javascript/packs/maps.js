2764d4515b020de261448f53cf52697da77da324
MandyWade0312 <amanda.wade@kin.com>
2021-11-15 16:11:53 -0500

    Adding the map url to the js

--- app/javascript/packs/maps.js
+++ app/javascript/packs/maps.js
@@ -1,6 +1,45 @@
 import L from "leaflet";
 
 
-const map = L.map('map').setView([51.505, -0.09], 13);
+document.addEventListener("turbolinks:load", () => {
+    const mapElement = document.querySelector("#map");
+    if (mapElement) {
+        const map = L.map(mapElement).setView([51.505, -0.09], 13);
 
-const image = L.imageOverlay(mapImage, bounds).addTo(map);
+        const bounds = [
+            [
+              [-26.5, -25],
+              [1021.5, 1023],
+            ],
+          ];
+        const image = L.imageOverlay(mapElement.dataset.mapUrl, bounds).addTo(map);   
+        console.log(mapElement.dataset.mapUrl);
+    }
+    else {
+        console.log("No Maps!");
+    }
+})
+
+// const map = L.map("map", {
+//     crs: L.CRS.Simple,
+//   });
+//   const bounds = [
+//     [
+//       [-26.5, -25],
+//       [1021.5, 1023],
+//     ],
+//   ];
+//   const image = L.imageOverlay(mapImage, bounds).addTo(map);
+//   map.fitBounds(bounds);
+
+//   fetch("/locations")
+//     .then((r) => r.json())
+//     .then((locations) => {
+//       locations.forEach((location) => {
+//         const coord = L.latLng([location.x, location.y]);
+//         const marker = L.marker(coord, { title: location.title });
+//         marker.location = location;
+//         marker.addTo(map);
+//         marker.on("click", handleClick);
+//       });
+//     });
