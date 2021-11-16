56cb443537a590b66ba8c9f0abfd669f378ba7e4
MandyWade0312 <amanda.wade@kin.com>
2021-11-16 15:20:35 -0500

    Added the aspect_ratio method

--- app/models/park.rb
+++ app/models/park.rb
@@ -6,4 +6,12 @@ class Park < ApplicationRecord
     validates :year_built, presence: true
 
     has_one_attached :map
+
+    def aspect_ratio 
+    #  if map_width < map_height
+    #     map_width.to_f / map_height.to_f
+    #  else
+        map_height.to_f / map_width.to_f
+    #  end
+    end
 end
